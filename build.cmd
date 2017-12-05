740fcaa587510f2408ee0691255396f8ef5f352d
HeXiongjun <373466062@qq.com>
2017-12-05 14:25:39 +0800

    Add the build instruction

--- build.cmd
+++ build.cmd
@@ -12,5 +12,9 @@ cd ../examples/Console
 cmake .
 make
 
+## Manually build the Console example program
+g++ -DNDEBUG main.cpp -o sio_console_demo -rdynamic libsioclient.a -lpthread -Wl,-Bstatic -lboost_system -lboost_date_time -lboost_random -Wl,-Bdynamic -std=gnu++11 -Wl,-rpath,/home/hexiongjun/Projects/TelinkBLETest/Socket.IO/socket.io-client-cpp/build/lib/Release -I /home/hexiongjun/Projects/TelinkBLETest/Socket.IO/socket.io-client-cpp/build/include
+
+
 ## Verbose build
 make VERBOSE=1
