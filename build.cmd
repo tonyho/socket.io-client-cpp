5baa70cca83e4db4332fcfb2b167a170ed2ebdb2
HeXiongjun <373466062@qq.com>
2017-12-05 14:16:10 +0800

    Update build commands

--- build.cmd
+++ build.cmd
@@ -1,6 +1,16 @@
+### Build the library
 mkdir build
 cd build
 cmake ..
 sudo apt-get install libwebsocketpp-dev 
 sudo apt-get install rapidjson-dev 
+sudo apt-get install libboost-dev libboost-date-time-dev libboost-random-dev libboost-system-dev libssl-dev
 make -j5
+
+## Build the example
+cd ../examples/Console
+cmake .
+make
+
+## Verbose build
+make VERBOSE=1
